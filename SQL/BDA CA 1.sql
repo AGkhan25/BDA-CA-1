395b41ada83b08193a031c0c97868c32a953d88a
AGkhan25 <sba25016@student.cct.ie>
2026-05-24 19:56:48 +0100

    update ALTER table

--- SQL/BDA CA 1.sql
+++ SQL/BDA CA 1.sql
@@ -41,3 +41,25 @@ su.Username
 FROM series2 As s
 Right JOIN useractivityseries AS si ON s.series_ID= si.series_ID
 Right JOIN users As su ON si.Username= su.Username;
+
+-- ●	Modify the database to include year details for movies 
+-- and series in the appropriate tables, using synthetic data.
+-- movies_year column in movies_yeartable which can be jopin together using inner jpinh 
+-- Inner Join is already presented from line 6 to line 13 above.
+-- We can also Alter the orignal table by creating new column in this and update that column
+ALTER TABLE movies3 ADD column movies_year YEAR;
+UPDATE movies3 m
+JOIN movies_year mi
+ON m.movie_ID = mi.movie_ID
+SET m.movies_year= mi.movies_year;
+-- to remove error in update column of 1175 take help form chat gpt 
+SET SQL_SAFE_UPDATES = 0;
+
+
+
+
+
+
+
+
+
